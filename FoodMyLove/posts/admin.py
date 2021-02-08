9cbf257c24b961796e00f8831708b7ad388d0520
florinchiruro <chiru.florin@ymail.com>
2021-02-08 20:54:08 +0200

    08.02.2021

--- FoodMyLove/posts/admin.py
+++ FoodMyLove/posts/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Category
 
 # Register your models here.
 
@@ -15,3 +15,4 @@ class Meta:
 
 
 admin.site.register(Post, AdminPost)
+admin.site.register(Category)
--- FoodMyLove/posts/models.py
+++ FoodMyLove/posts/models.py
@@ -1,6 +1,19 @@
 from django.db import models
 from django.conf import settings
+from django.template.defaultfilters import slugify
 # Create your models here.
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=150)
+    slug = models.SlugField(editable=False)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Category, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
 
 
 class Post(models.Model):
@@ -10,3 +23,12 @@ class Post(models.Model):
     image = models.ImageField(blank=True, null=True, upload_to='uploads/')
     user = models.ForeignKey(settings.AUTH_USER_MODEL,
                              on_delete=models.CASCADE)
+    slug = models.SlugField(default='slug', editable=False)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Post, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
